bf98cb79b6e176d119481835139c35ac935fa218
Diego <dapoloniov1600@alumno.ipn.mx>
2020-04-22 01:07:51 -0500

    Data structures exercise #4 done

---
+++ codigofacilito-python-course/data_structures_exercises/exercise_4.py
@@ -0,0 +1,25 @@
+# Given a list of phrases entered by the user, show on screen
+# all those that are palindromes.
+# ===========================================================
+
+def is_palindrome(phrase):
+    phrase = phrase.lower().replace(' ', '')
+    return phrase == phrase[::-1]
+
+if __name__ == '__main__':
+    users_phrases = list()
+    number_of_phrases = int(input('\nEnter the number of phrases you want to enter: '))
+    current_phrase = ''
+
+    for i in range(number_of_phrases):
+        current_phrase = input('\nEnter phrase #{}: '.format(i + 1))
+        users_phrases.append(current_phrase.capitalize())
+
+    print('\n=================================')
+    print('\nThe phrases: ')
+
+    for phrase in users_phrases:
+        if is_palindrome(phrase):
+            print('\n- {}'.format(phrase))
+
+    print('\nAre palindromes.')
