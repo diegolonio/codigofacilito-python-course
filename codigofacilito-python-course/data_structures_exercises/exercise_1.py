555200dab7e8cae6e88f1061bd5e9c2558774b48
Diego <dapoloniov1600@alumno.ipn.mx>
2020-04-22 00:15:39 -0500

    Added more readability

--- codigofacilito-python-course/data_structures_exercises/exercise_1.py
+++ codigofacilito-python-course/data_structures_exercises/exercise_1.py
@@ -1,14 +1,15 @@
 # Given a dictionary which contains the student's notes, the keys are the subjects'
-# names and the values are the subjects' notes. Show on screen the studen's average note.
+# names and the values are the subjects' notes. Show on screen the student's average note.
 # =======================================================================================
 
-def average_note(dictionary):
-    return sum(dictionary.values()) / len(dictionary)
+def average_note(students_notes):
+    return sum(students_notes.values()) / len(students_notes)
 
 if __name__ == '__main__':
-    my_dict = {'Maths': None, 'Literature': None, 'History': None, 'Chemistry': None, 'Biology': None}
+    students_notes = {'Maths': None, 'Literature': None, 'History': None, 'Chemistry': None, 'Biology': None}
 
-    for key, value in my_dict.items():
-        my_dict[key] = int(input('\nPlease enter the note for {} subject: '.format(key)))
+    for subject in students_notes:
+        students_notes[subject] = int(input('\nPlease enter the note for {} subject: '.format(subject)))
 
-    print('\n{}\n'.format(average_note(my_dict)))
+    print('\n==================================')
+    print('\nThe student\'s average note is: {}\n'.format(average_note(students_notes)))
